a42656e3df359fd728730f479e5436104276506e
unknown <zamzamabdullahi69@gmail.com>
2024-11-03 10:23:59 +0300

    updateTask

--- controllers/taskControllers.js
+++ controllers/taskControllers.js
@@ -52,3 +52,58 @@ exports.createTask = (req, res) => {
     })
 }
 
+exports.updateTask = (req, res) => {
+    const form = new IncomingForm();
+    form.parse(req, (err, fields, files) => {
+        if(err) {
+            res.writeHead(400, { 'content-type': 'application/json'});
+            res.end(JSON.stringify({
+                message: 'Error parsing form'
+            }))
+            return;
+        }
+
+        if (!fields.title) {
+            res.writeHead(400, { 'content-type': 'application/json'});
+            res.end(JSON.stringify({
+                message: 'Title is required'
+            }))
+            return;
+        }
+
+        const image = files.image ? files.image[0] : null;
+
+        const tasks = readTasksFromFile()
+
+        const taskId = parseInt(req.url.split('/').pop());
+        const taskIndex = tasks.findIndex(task => task.id === taskId);
+
+        if (taskIndex === -1) {
+            res.writeHead(404, { 'content-type': 'application/json'});
+            res.end(JSON.stringify({
+                message: 'Task not found'
+            }))
+            return;
+        }
+
+        const updatedTask = {
+            ...tasks[taskIndex],
+            title: fields.title || tasks[taskIndex].title,
+            description: fields.description || tasks[taskIndex].description,
+            status: fields.status || tasks[taskIndex].status,
+            image: image ? `/uploads/${image.originalFilename}` : tasks[taskIndex].image,
+        }
+
+        tasks[taskIndex] = updatedTask;
+
+        writeTasksToFile(tasks);
+
+        if(image) {
+            fs.copyFileSync(image.filepath, path.join(__dirname, '../uploads', image.originalFilename));
+        }
+
+        res.writeHead(200, { 'content-type': 'application/json'});
+        res.end(JSON.stringify(updatedTask))
+    })
+}
+
